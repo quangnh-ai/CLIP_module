84ce03d4334a13d60bce7fd3b63d8857e6d9af47
unknown <quang009182@gmail.com>
2021-09-22 21:37:06 +0700

    change save format to h5

--- extract_keyframes.py
+++ extract_keyframes.py
@@ -3,12 +3,14 @@ import os
 import argparse
 from tqdm import tqdm
 import pandas as pd
+import h5py
 
 from utils.model import ExtractorModule
 
 def get_arg():
     parser = argparse.ArgumentParser()
     parser.add_argument('--keyframe_folder_path', type=str)
+    parser.add_argument('--save_path', type=str)
     parser.add_argument('--cuda', action='store_true', default=False)
     parser.add_argument('--batch_size', type=int, default=50) 
     args = parser.parse_args()
@@ -29,7 +31,7 @@ if __name__ == '__main__':
     shot_folder_list = os.listdir(keyframe_folder_path)
     shot_folder_list = sorted(shot_folder_list)
 
-    keyframe_id = []
+    keyframe_ids = []
     keyframes_features = torch.empty([0, 512], dtype=torch.float16).to(device)
 
     for folder in tqdm(shot_folder_list):
@@ -38,14 +40,20 @@ if __name__ == '__main__':
         features = extractor.ExtractKeyframes(folder_keyframe_path=folder_path, batch_size=batch_size)
 
         keyframes_features = torch.cat((keyframes_features, features))
-        keyframe_id += ids
+        keyframe_ids += ids
     
     print('||||Finised extract features||||')
     print('||||Saving Features........||||')
     keyframes_features = keyframes_features.tolist()
 
-    data = pd.DataFrame(zip(keyframe_id, keyframes_features), columns=['id', 'features'])
-    data.to_csv('keyframes_features.csv', index=None)
+    # data = pd.DataFrame(zip(keyframe_id, keyframes_features), columns=['id', 'features'])
+    # data.to_csv('keyframes_features.csv', index=None)
+    save_path = args.save_path
+    data = h5py.File(save_path, 'w')
+    data.create_dataset('ids', data=keyframe_ids)
+    data.create_dataset('features', data=keyframes_features)
+    data.close()
+
     print('____FINISHED____')
 
 
