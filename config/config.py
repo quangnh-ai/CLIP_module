1bf5e558289f4969a22d3fba2f2e1528c43ff6be
unknown <quang009182@gmail.com>
2021-09-30 15:03:42 +0700

    update config for module

--- config/config.py
+++ config/config.py
@@ -3,11 +3,8 @@ import os
 
 def init_config(config_path=None) -> ConfigParser:
     config = ConfigParser()
-    if not config_path:
-        env = os.getenv("ENVIRONMENT")
-        if not env:
-            raise 'must set environment'
-        config_path = 'configs/{}.ini'.format(env)
+
+    config_path = 'configs/config.ini'
 
     config.read(config_path)
     return config
