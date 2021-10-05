d61c2db86e53bcd8d519396a4a609ab9479a0f5a
unknown <quang009182@gmail.com>
2021-10-05 15:22:29 +0700

    add API code

---
+++ app.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import uvicorn
+import h5py
+
+from configs.config import init_config
+from utils.retrieval_model import RetrievalModel
+from utils.retrieval_model import get_arg
+
+
+app = FastAPI()
+my_cfg = init_config()
+
+class Request(BaseModel):
+    query: str
+
+@app.post("/retrieval")
+def retrieval(req: Request):
+    req = req.dict()
+    query = req["query"]
+    return {"result": retrieval_model.retrieval(query)}
+    
+if __name__ == "__main__":
+    retrieval_model = RetrievalModel(get_arg(), my_cfg)
+    uvicorn.run(app, host="0.0.0.0", port=3000)
---
+++ utils/retrieval_model.py
@@ -0,0 +1,59 @@
+import clip
+import numpy as np
+import torch
+import cv2
+import os
+from PIL import Image
+import math
+from tqdm import tqdm
+
+import argparse
+import h5py
+
+from configs.config import init_config
+
+def get_arg():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--features_path', type=str)
+
+    args = parser.parse_args()
+    return args
+
+class RetrievalModel:
+    def __init__(self, args, my_cfg) -> None:
+        
+        self.data = h5py.File(args.features_path, 'r')
+        self.model_name = my_cfg["CLIP"]["model_name"]
+        self.device = my_cfg["device"]["device"]
+
+        self.features = self.data.get('features')
+        self.features = np.array(self.features)
+        self.features = torch.from_numpy(self.features).to(self.device)
+        self.features = self.features.to(torch.float16)
+
+        self.count = len(self.features)
+
+        self.ids = self.data.get('ids')
+        self.ids = np.array(self.ids)
+        for i in range(len(self.ids)):
+            self.ids[i] = self.ids[i].decode('utf-8')
+
+        self.model, self.preprocess = clip.load(self.model_name, device=self.device)
+
+    def retrieval(self, query):
+        
+        with torch.no_grad():
+            text_features = self.model.encode_text(clip.tokenize(query).to(self.device))
+            text_features /= text_features.norm(dim=-1, keepdim=True)
+
+        print(self.features.shape)
+
+        similarities = (100 * self.features @ text_features.T)
+        values, best_frame_idx = similarities.topk(self.count, dim=0)
+
+        results = []
+
+        for i in best_frame_idx:
+            results.append(self.ids[i])
+        
+        return results
