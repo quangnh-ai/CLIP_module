da16626987b2920bd678f459d754963f284d4a1c
unknown <quang009182@gmail.com>
2021-10-18 20:03:25 +0700

    finish api code for indexing feature

--- indexing_api.py
+++ indexing_api.py
@@ -1,17 +1,25 @@
 import faiss
-from app import retrieval
+from fastapi import FastAPI
+from pydantic import BaseModel
+import uvicorn
 
 from utils.feature_indexing import get_arg
 from utils.feature_indexing import IndexingRetrievalModel
 from configs.config import init_config
 
-
+app = FastAPI()
 my_cfg = init_config()
 
-if __name__ == '__main__':
-    
-    retrieval_model = IndexingRetrievalModel(get_arg(), my_cfg)
-    query = input("Query: ")
-    result = retrieval_model.retrieval(query)
+class Request(BaseModel):
+    query: str
 
-    print(result)
+@app.post("/retrieval")
+def retrieval(req: Request):
+    req = req.dict()
+    query = req["query"]
+    return {"result": retrieval_model.retrieval(query)}
+
+if __name__ == '__main__':
+    retrieval_model = IndexingRetrievalModel(get_arg(), my_cfg)
+    uvicorn.run(app, host="0.0.0.0", port=5000)
+
